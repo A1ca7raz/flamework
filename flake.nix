55231fed554e8520e711198ba1a5924de5b1d545
a1ca7raz <7345998+a1ca7raz@users.noreply.github.com>
2023-04-08 00:02:49 +0800

    Flamework2: migrate to flake-utils

--- flake.nix
+++ flake.nix
@@ -3,65 +3,65 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    impermanence.url = "github:nix-community/impermanence";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # Other flake utils
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+
+    # NixosModule Flakes
     colmena = {
       url = "github:zhaofengli/colmena";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-utils.follows = "flake-utils";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-      # inputs.utils.follows = "flake-utils";
+      inputs.utils.follows = "flake-utils";
     };
-    # lanzaboote = {
-    #   url = "github:nix-community/lanzaboote";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    #   inputs.flake-utils.follows = "flake-utils";
-    # };
-    # flake-utils.url = "github:numtide/flake-utils";
+    impermanence.url = "github:nix-community/impermanence";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-utils ... }:
     let
+      SYSTEM = [ "x86_64-linux" ];
+
       lib = nixpkgs.lib;
       utils = import ./utils lib self;
-      nur = import ./pkgs;
-
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config = {
-          allowUnfree = true;
+    in flake-utils.lib.eachSystem SYSTEM (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+          overlays = [ nur.overlay ] ++ utils.loader.overlays;
         };
-        overlays = [ nur.overlay ] ++ utils.loader.overlays;
-      };
-    in {
-      packages = nur.packages pkgs;
-      legacyPackages = pkgs;
-
-      overlays.default = nur.overlay;
-
-      devShells.x86_64-linux.default = with pkgs; mkShell {
-        nativeBuildInputs = [ colmena nvfetcher ];
-      };
-
+      in rec {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = with pkgs; mkShell { nativeBuildInputs = [ colmena nvfetcher ]; };
+      }
+    ) // {
       nixosModules = utils.module // utils.loader.nixosModules // (with inputs; {
         sops = sops-nix.nixosModules.sops;
         impermanence = impermanence.nixosModules.impermanence;
-        disko = disko.nixosModules.disko;
         home = home-manager.nixosModules.home-manager;
-        lanzaboote = lanzaboote.nixosModules.lanzaboote;
       });
 
       nixosConfigurations = utils.loader.profiles.nixosConfigurations;
-
       colmena = utils.loader.profiles.colmena;
     };
 }
