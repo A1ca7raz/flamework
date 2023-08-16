8db4944b75a3f0716b9ce359e90193ccc44da0a4
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-08-16 20:57:47 +0800

    migrate windeco rc to overlayfile and add kwinrules

--- modules/desktop/beautify/window-decoration.nix
+++ modules/desktop/beautify/window-decoration.nix
@@ -1,35 +1,13 @@
-{ home, pkgs, lib, util, ... }:
-let
-  _wc = util.wrapWC pkgs;
-  wc = _wc "sierrabreezeenhancedrc";
-  wcdeo = wc "Windeco";
+{
+  nixosModule = { util, user, ... }:
+  with util; mkOverlayModule user {
+    sierrabreezeenhancedrc = {
+      target = c "sierrabreezeenhancedrc";
+      source = ./sierrabreezeenhancedrc;
+    };
+  };
 
-  mkShadow = id: n: ''
-    echo -e "[Windeco Exception ${toString id}]\n\
-    BorderSize=2\n\
-    DrawBackgroundGradient=false\n\
-    DrawTitleBarSeparator=false\n\
-    Enabled=true\n\
-    ExceptionPattern=${n}\n\
-    ExceptionType=0\n\
-    GradientOverride=-1\n\
-    HideTitleBar=3\n\
-    IsDialog=false\n\
-    Mask=0\n\
-    MatchColorForTitleBar=true\n\
-    OpacityOverride=-1\n\
-    OpaqueTitleBar=false" >> $HOME/.config/sierrabreezeenhancedrc
-  '';
-in {
-  home.packages = [ pkgs.sierra-breeze-enhanced ];
-
-  home.activation.setupWindeco = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${wc "Common" "ShadowSize" "ShadowSmall"}
-    ${wcdeo "ButtonStyle" "sbeDarkAuroraeActive"}
-    ${wcdeo "DrawBackgroundGradient" "false"}
-    ${wcdeo "DrawTitleBarSeparator" "false"}
-    ${wcdeo "OpaqueTitleBar" "false"}
-    ${wcdeo "UnisonHovering" "false"}
-    ${mkShadow 0 "steam"}
-  '';
+  homeModule = { pkgs, ... }: {
+    home.packages = [ pkgs.sierra-breeze-enhanced ];
+  };
 }
