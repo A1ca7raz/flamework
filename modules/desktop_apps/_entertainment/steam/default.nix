780362034ccf56ebe185fba71d36f3737311ebdc
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-09-12 16:02:15 +0800

    proton: fix steam compattool

--- modules/desktop_apps/_entertainment/steam/default.nix
+++ modules/desktop_apps/_entertainment/steam/default.nix
@@ -1,12 +1,18 @@
 {
   homeModule = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      steam-run
-      protonup-ng
-    ];
+    home = {
+      packages = with pkgs; [
+        steam-run
+        protonup-ng
+      ];
+
+      sessionVariables = {
+        STEAM_EXTRA_COMPAT_TOOLS_PATHS = "$HOME/.steam/root/compatibilitytools.d";
+      };
+    };
   };
 
-  nixosModule = { user, ... }: {
+  nixosModule = { user, util, ... }: {
     programs.steam.enable = true;
     environment.persistence."/nix/persist".users.${user}.directories = [
       ".local/share/Steam"
