5c3c6539f6a44e444e689de34d10c462daf64455
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-09-12 19:16:42 +0800

    disable unused inputs

--- flake.nix
+++ flake.nix
@@ -15,10 +15,10 @@
       inputs.flake-compat.follows = "flake-compat";
       inputs.flake-utils.follows = "flake-utils";
     };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # disko = {
+    #   url = "github:nix-community/disko";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
