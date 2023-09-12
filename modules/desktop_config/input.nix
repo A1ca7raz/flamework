069e7e7f89cd1ea446471b854d34a0631ff29939
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-09-12 16:08:55 +0800

    wayland: add wayland input settings

--- modules/desktop_config/input.nix
+++ modules/desktop_config/input.nix
@@ -1,10 +1,16 @@
 { util, lib, pkgs, ... }:
 let
   wc = util.wrapWC pkgs "kcminputrc";
+  wc_ = util.wrapWC_ pkgs "$HOME/.config/kcminputrc";
+  wctouchpad = wc_ ["Libinput" "1739" "52781" "MSFT0004:00 06CB:CE2D Touchpad"];
 in {
   home.activation.setupInput = lib.hm.dag.entryAfter ["writeBoundary"] ''
     ${wc "Keyboard" "NumLock" "0"}
     ${wc "Mouse" "X11LibInputXAccelProfileFlat" "false"}
     ${wc "Mouse" "XLbInptAccelProfileFlat" "true"}
+    ${wc_ ["Libinput" "1133" "16505" "Logitech G Pro "] "PointerAccelerationProfile" "1"}
+    ${wctouchpad "ClickMethod" "2"}
+    ${wctouchpad "NaturalScroll" "true"}
+    ${wctouchpad "TapToClick" "true"}
   '';
 }
