a9d43afd9ad48aead8e38ddb2ed9e4ed73090477
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-09-14 09:10:27 +0800

    wayland: add another mouse

--- modules/desktop_config/input.nix
+++ modules/desktop_config/input.nix
@@ -3,12 +3,14 @@ let
   wc = util.wrapWC pkgs "kcminputrc";
   wc_ = util.wrapWC_ pkgs "$HOME/.config/kcminputrc";
   wctouchpad = wc_ ["Libinput" "1739" "52781" "MSFT0004:00 06CB:CE2D Touchpad"];
+  wcmouse = x: wc_ (["Libinput"] ++ x) "PointerAccelerationProfile" "1";
 in {
   home.activation.setupInput = lib.hm.dag.entryAfter ["writeBoundary"] ''
     ${wc "Keyboard" "NumLock" "0"}
     ${wc "Mouse" "X11LibInputXAccelProfileFlat" "false"}
     ${wc "Mouse" "XLbInptAccelProfileFlat" "true"}
-    ${wc_ ["Libinput" "1133" "16505" "Logitech G Pro "] "PointerAccelerationProfile" "1"}
+    ${wcmouse ["1133" "16505" "Logitech G Pro "]}
+    ${wcmouse ["1133" "16500" "Logitech G304"]}
     ${wctouchpad "ClickMethod" "2"}
     ${wctouchpad "NaturalScroll" "true"}
     ${wctouchpad "TapToClick" "true"}
