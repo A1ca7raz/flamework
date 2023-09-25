7f00c56ca3b77beb3bca32d75535e04400fac0ab
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-09-25 21:35:18 +0800

    lib: add mkHomePkg function

--- utils/lib/module_helper.nix
+++ utils/lib/module_helper.nix
@@ -1,22 +1,31 @@
 { lib, ... }:
-{
+rec {
+  mkPersistDirsTree = user: dirs: {
+    "/nix/persist".users.${user}.directories = dirs;
+  };
+
   mkPersistDirsModule = user: dirs: {
-    environment.persistence."/nix/persist".users.${user}.directories = dirs;
+    environment.persistence = mkPersistDirsTree user dirs;
   };
 
   mkPersistFilesModule = user: files: {
     environment.persistence."/nix/persist".users.${user}.files = files;
   };
 
-  mkPersistDirsTree = user: dirs: {
-    "/nix/persist".users.${user}.directories = dirs;
-  };
-
-  mkOverlayModule = user: sets: {
-    environment.overlay.users.${user} = sets;
-  };
 
   mkOverlayTree = user: sets: {
     users.${user} = sets;
+  };
+
+  mkOverlayModule = user: sets: {
+    environment.overlay = mkOverlayTree user sets;
+  };
+
+  mkHomePackagesTree = list: {
+    packages = list;
+  };
+
+  mkHomePackagesModule = list: {
+    home = mkHomePackagesTree list;
   };
 }
