806af72122afd10da823fe579b5645460872c885
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-10-02 15:29:25 +0800

    latte: use systemd user service to keep latte alive

--- modules/desktop_config/autostart.nix
+++ modules/desktop_config/autostart.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 let
   delay' = s: x: ''sh -c "sleep ${toString s}; exec ${x}"'';
   delay = delay' 2;
@@ -7,10 +7,27 @@ in {
     keepassxc = delay' 10 "keepassxc";
     # telegram = delay "env telegram-desktop -autostart";
     yakuake = delay "yakuake";
-    latte-dock = delay "latte-dock";
     steam = delay "steam -silent";
     thunderbird = delay "birdtray";
     easyeffects = delay "easyeffects --gapplication-service";
-    # clash = delay' 10 "clash-verge";
+    # latte-dock = delay "latte-dock";
+  };
+
+  systemd.user.services.latte-dock-autostart = with pkgs; let
+    lattePackage = latte-dock-nostartup; 
+  in {
+    Unit = {
+      Description = "Keep latte alive";
+      After = [ "plasma-workspace.target" "plasma-plasmashell.service" ];
+    };
+    Service = {
+      ExecStop = "${procps}/bin/pkill latte";
+      ExecStart = "${lattePackage}/bin/latte-dock";
+      Type = "idle";
+      Restart = "on-failure";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
   };
 }
