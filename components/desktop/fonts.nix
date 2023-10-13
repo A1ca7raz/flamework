89c1911ba3b758b0d889bc722061b985dd29dbdc
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-10-13 23:42:46 +0800

    font: remove unused fonts

--- components/desktop/fonts.nix
+++ components/desktop/fonts.nix
@@ -5,28 +5,20 @@
   fonts.packages = with pkgs; [
     my-nerd-font-pack
 
-    # corefonts
-    # fira-code
     fira-code-symbols
-    # fira-mono
     font-awesome
-    hanazono
-    # jetbrains-mono
+
     liberation_ttf
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
     noto-fonts-emoji-blob-bin
     noto-fonts-extra
-    # roboto-mono
     sarasa-gothic
     source-code-pro
     source-han-mono
     source-han-sans
     source-han-serif
-    # ubuntu_font_family
-    # vistafonts
-    # vistafonts-chs
     wqy_microhei
   ];
 
