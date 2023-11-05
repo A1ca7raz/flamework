3c869f2b1cdfb0942f92eb4bd300291963faa108
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-11-05 09:53:16 +0800

    pipewire: try to fix bt audio

--- components/desktop/pipewire.nix
+++ components/desktop/pipewire.nix
@@ -1,5 +1,12 @@
-{ ... }:
-{
+{ pkgs, ... }:
+let
+  mkLimit = a: b: c: d: {
+    domain = a;
+    type = b;
+    item = c;
+    value = d;
+  };
+in {
   security.rtkit.enable = true;
 
   services.pipewire = {
@@ -10,20 +17,24 @@
     alsa.support32Bit = true;
     jack.enable = true;
     pulse.enable = true;
-
-    # config = {
-    #   client = {
-    #     "stream.properties"."resample.quality" = 10;
-    #   };
-    #   client-rt = {
-    #     "stream.properties"."resample.quality" = 10;
-    #   };
-    #   jack = {
-    #     "stream.properties"."resample.quality" = 10;
-    #   };
-    #   pipewire-pulse = {
-    #     "stream.properties"."resample.quality" = 10;
-    #   };
-    # };
   };
+
+  # Realtime Priority
+  systemd.services.rtkit-daemon.serviceConfig.ExecStart = [
+    ""
+    "${pkgs.rtkit}/libexec/rtkit-daemon --no-canary"
+  ];
+
+  security.pam.loginLimits = [
+    (mkLimit "@realtime" "-" "rtprio" "98")
+    (mkLimit "@realtime" "-" "memlock" "unlimited")
+    (mkLimit "@realtime" "-" "nice" "-11")
+  ];
+
+  services.udev.extraRules = ''
+    # rw access to /dev/cpu_dma_latency to prevent CPUs from going into idle state
+    KERNEL=="cpu_dma_latency", GROUP="realtime"
+  '';
+
+  users.groups.realtime = {};
 }
