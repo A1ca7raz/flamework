77f5aa8456a609ea005897ee58ac31d59f4df3a3
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-11-05 10:00:02 +0800

    autostart: fix

--- modules/desktop_config/autostart.nix
+++ modules/desktop_config/autostart.nix
@@ -1,30 +1,33 @@
 { pkgs, ... }:
 let
   delay' = s: x: ''sh -c "sleep ${toString s}; exec ${x}"'';
-  delay = delay' 2;
+  delay2 = delay' 2;
+  delay5 = delay' 5;
+  delay10 = delay' 10;
 in {
   utils.startup = {
-    keepassxc = delay' 10 "keepassxc";
-    # telegram = delay "env telegram-desktop -autostart";
-    yakuake = delay "yakuake";
-    steam = delay "steam -silent";
-    thunderbird = delay "birdtray";
-    easyeffects = delay "easyeffects --gapplication-service";
-    # latte-dock = delay "latte-dock";
+    keepassxc = delay10 "keepassxc";
+    # telegram = delay2 "env telegram-desktop -autostart";
+    yakuake = delay2 "yakuake";
+    steam = delay10 "steam -silent";
+    thunderbird = delay2 "birdtray";
+    easyeffects = delay2 "easyeffects --gapplication-service";
   };
 
   systemd.user.services.latte-dock-autostart = with pkgs; let
-    lattePackage = latte-dock-nostartup; 
+    lattePackage = latte-dock-nostartup;
   in {
     Unit = {
       Description = "Keep latte alive";
-      After = [ "plasma-workspace.target" "plasma-plasmashell.service" ];
+      After = "graphical-session.target";
+      PartOf = "graphical-session.target";
     };
     Service = {
       ExecStop = "${procps}/bin/pkill latte";
       ExecStart = "${lattePackage}/bin/latte-dock";
       Type = "idle";
       Restart = "on-failure";
+      Slice = "app.slice";
     };
     Install = {
       WantedBy = [ "default.target" ];
