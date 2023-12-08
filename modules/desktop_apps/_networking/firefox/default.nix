f638a239e59b71d667fed89d787c02afd8af65c7
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-12-08 10:03:00 +0800

    firefox: update nativeMessagingHosts

--- modules/desktop_apps/_networking/firefox/default.nix
+++ modules/desktop_apps/_networking/firefox/default.nix
@@ -2,17 +2,17 @@
   nixosModule = { util, user, ... }:
   with util; mkPersistDirsModule user [
     ".mozilla/firefox"
-    ".mozilla/native-messaging-hosts"
     (ls "tor-browser")
   ];
 
   homeModule = { pkgs, ... }: {
     home.packages = with pkgs; [
       (firefox.override {
-        cfg = {
-          enablePlasmaBrowserIntegration = true;
-          smartcardSupport = true;
-        };
+        nativeMessagingHosts = [
+          plasma-browser-integration
+          keepassxc
+        ];
+        cfg.smartcardSupport = true;
       })
       tor-browser
     ];
