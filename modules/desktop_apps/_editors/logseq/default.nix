cd6101fbb67c898a209d49210326ab1544095fd6
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-12-21 18:03:05 +0800

    logseq: allow insecure electron runtime

--- modules/desktop_apps/_editors/logseq/default.nix
+++ modules/desktop_apps/_editors/logseq/default.nix
@@ -3,8 +3,12 @@
     home.packages = [ pkgs.logseq ];
   };
 
-  nixosModule = { user, util, ... }:
-  with util; mkPersistDirsModule user [
-    (c "Logseq") ".logseq"
-  ];
+  nixosModule = { user, util, ... }: {
+    environment.persistence = with util; mkPersistDirsTree user [
+      (c "Logseq") ".logseq"
+    ];
+    nixpkgs.config.permittedInsecurePackages = [
+      "electron-25.9.0"
+    ];
+  };
 }
