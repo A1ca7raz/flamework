fe9214dbac260c45bc02fa5b7245b0e1cecb2587
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-12-24 19:24:36 +0800

    sing-box: implement subscription updater

--- modules/desktop_apps/_networking/sing-box/default.nix
+++ modules/desktop_apps/_networking/sing-box/default.nix
@@ -1,50 +1,64 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 with lib; let
   enable = true;
-  configFile = "/home/nomad/.config/sing-box/config.json";
-  webUi = {
-    package = pkgs.clash-webui-yacd-meta;
-  };
+  configFile = "$STATE_DIRECTORY/config.json";
+  subscriptionURI = config.sops.secrets.sing-box.path;
+
+  uiPackage = pkgs.clash-webui-yacd-meta;
 in {
+  sops.secrets.sing-box = {
+    mode = "0700";
+    owner = "sing-box";
+    group = "sing-box";
+  };
+
+  users.users."sing-box" = {
+    isSystemUser = true;
+    group = "sing-box";
+    description = "Sing-box daemon user";
+    home = "/var/lib/sing-box";
+  };
+
+  users.groups."sing-box" = {};
+
   environment.persistence."/nix/persist".users.nomad.directories = [ ".config/sing-box" ];
 
   systemd.services.sing-box = optionalAttrs enable (
     with pkgs; let
-    startScript = pkgs.writeShellScriptBin "sing-box-start" ''
-      CONF_DIR=${"\$\{STATE_DIRECTORY:-/var/lib/sing-box}"}
-      CONF=$1
-      echo "Config Path: $CONF"
-      mkdir -p $CONF_DIR
+      startScript = pkgs.writeShellScriptBin "sing-box-start" ''
+        CONF_DIR=${"\$\{STATE_DIRECTORY:-/var/lib/sing-box}"}
+        CONF=${configFile}
+        mkdir -p $CONF_DIR
 
-      ${sing-box}/bin/sing-box run -D $CONF_DIR -c $CONF
-    '';
+        curl -L `cat ${subscriptionURI}` -o $CONF
+        chmod 0700 $CONF
+        ${sing-box}/bin/sing-box run -D $CONF_DIR -c $CONF
+      '';
 
-    caps = [
-      "CAP_NET_ADMIN"
-      "CAP_NET_BIND_SERVICE"
-    ];
+      caps = [
+        "CAP_NET_ADMIN"
+        "CAP_NET_BIND_SERVICE"
+      ];
     in {
       description = "Sing-box networking service";
-      path = [ coreutils ];
-      # Don't start if the config file doesn't exist.
-      unitConfig = {
-        # NOTE: configPath is for the original config which is linked to the following path.
-        ConditionPathExists = configFile;
-      };
+      path = [ coreutils curl ];
 
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
 
       serviceConfig = {
         Type = "simple";
-        LoadCredential = "config:${configFile}";
-        ExecStart = "${getExe startScript} %d/config";
-        Restart = "on-failure";
+        ExecStart = "${getExe startScript}";
         StateDirectory = "sing-box";
         CapabilityBoundingSet = caps;
         AmbientCapabilities = caps;
         NoNewPrivileges = "yes";
-        DynamicUser = "yes";
+        User = "sing-box";
+        Group = "sing-box";
+
+        # Sing-box auto update
+        Restart = "always";
+        RuntimeMaxSec = "12h";
       };
     }
   );
@@ -52,7 +66,7 @@ in {
   services.lighttpd = {
     inherit enable;
     port = 80;
-    document-root = "${webUi.package}/share/clash/ui";
+    document-root = "${uiPackage}/share/clash/ui";
     extraConfig = ''server.bind = "127.0.0.88"'';
   };
 
