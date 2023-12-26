7ce8408457639cf1a28cd2fefe5fa8adcf89dc44
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-12-27 00:32:36 +0800

    add template desktop

---
+++ profiles/__templates/desktop.nix
@@ -0,0 +1,22 @@
+{ self, ... }:
+let
+  components = self.nixosModules.components;
+  modules = self.nixosModules.modules;
+in {
+  # targetPort = 22;
+  targetUser = "nomad";
+  system = "x86_64-linux";
+
+  modules = with modules; [
+    common
+  ] ++ (with components; [
+    __common
+    desktop
+  ]);
+
+  # users."username".modules = with self.nixosModules; [];
+
+  extraConfig = { ... }: {
+    # ...
+  };
+}
