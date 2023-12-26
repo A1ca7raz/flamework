9de7cac45124b84a215b6d27c097b0f5ed605688
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-12-27 00:34:41 +0800

    update profile laptop

--- profiles/laptop/default.nix
+++ profiles/laptop/default.nix
@@ -1,24 +1,21 @@
-{ self, ... }:
+{ self, desktop, ... }:
 with self; let
   components = self.nixosModules.components;
   modules = self.nixosModules.modules;
-in {
+in desktop {
   targetHost = "192.168.10.3";
   targetPort = 22;
   targetUser = "nomad";
   # system = "x86_64-linux";
 
   modules = with modules; [
-    common
     desktop_apps
     desktop_beautify
     desktop_config
     user.nomad
     overlayfile
   ] ++ (with components; [
-    __common
     binary-cache-cn
-    desktop
     wayland
   ] ++ (with hardware; [
     amdcpu
