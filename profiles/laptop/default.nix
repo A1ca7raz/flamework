69e359c2b3daecca3a18202b8b15e44746b4fc4b
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-12-26 22:56:05 +0800

    upgrade profile laptop

--- profiles/laptop/default.nix
+++ profiles/laptop/default.nix
@@ -1,14 +1,22 @@
-{
-  components,
-  modules,
-  ...
-}: {
+{ self, ... }:
+with self; let
+  components = self.nixosModules.components;
+  modules = self.nixosModules.modules;
+in {
   targetHost = "192.168.10.3";
   targetPort = 22;
   targetUser = "nomad";
   # system = "x86_64-linux";
 
-  components.use = with components; [
+  modules = with modules; [
+    common
+    desktop_apps
+    desktop_beautify
+    desktop_config
+    user.nomad
+    overlayfile
+  ] ++ (with components; [
+    __common
     binary-cache-cn
     desktop
     wayland
@@ -17,18 +25,9 @@
     amdgpu
     bluetooth
     nvme
-  ]);
+  ]));
 
-  modules.use = with modules; [
-    common
-    desktop_apps
-    desktop_beautify
-    desktop_config
-    user.nomad
-    overlayfile
-  ];
-
-  extraConfiguration = { ... }: {
+  extraConfig = { ... }: {
     networking.hostName = "oxygenlaptop";
     environment.overlay.debug = false;
   };
