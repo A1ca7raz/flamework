e47a93c51dfd4fb7fd306248346f69dbb511ab02
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-01-07 09:13:25 +0800

    openssh: update config

--- modules/services/server/openssh/default.nix
+++ modules/services/server/openssh/default.nix
@@ -1,18 +1,33 @@
-{ ... }:
-{
+{ lib, ... }:
+let
+  PORTS = [ 48422 ];
+in {
   services.openssh = {
     enable = true;
-    settings.PermitRootLogin = "prohibit-password";
-    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = lib.mkForce "prohibit-password";
+    };
+    ports = PORTS;
     hostKeys = [
-      {
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-      {
-        path = "/etc/ssh/ssh_host_rsa_key";
-        type = "rsa";
-      }
+      { path = "/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
+      { path = "/etc/ssh/ssh_host_rsa_key"; type = "rsa"; }
     ];
+    extraConfig = ''
+      TrustedUserCAKeys /etc/ssh/ssh_user_ca.pub
+    '';
+  };
+
+  environment.persistence."/nix/persist".files = [
+    "/etc/machine-id"
+    "/etc/ssh/ssh_host_ed25519_key"
+    "/etc/ssh/ssh_host_ed25519_key.pub"
+    "/etc/ssh/ssh_host_rsa_key"
+    "/etc/ssh/ssh_host_rsa_key.pub"
+  ];
+
+  environment.etc.sshUserCA = {
+    target = "ssh/ssh_user_ca.pub";
+    text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIImEnstzrNsASzPhILySuXHjeyA84Hv0U1ini3w/4JBn";
   };
 }
