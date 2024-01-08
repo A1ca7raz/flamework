310d395cbc4fc9b34086e51a6e1c603730a01c0e
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-01-08 23:07:51 +0800

    add module postgresql

--- modules/services/server/postgresql/default.nix
+++ modules/services/server/postgresql/default.nix
@@ -1,12 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
   services.postgresql = {
     enable = true;
-    package = pkgs.postgresql_15;
+    package = pkgs.postgresql_16_jit;
+    initdbArgs = [ "--locale=zh_CN.UTF-8" "-E UTF8" "--data-checksums" ];
 
-    initdbArgs = [
-      "--locale=zh_CN.UTF-8"
-      "-E UTF8"
-    ];
+    settings = {
+      listen_addresses = with lib; mkForce (concatStringsSep ", " [
+        "127.0.0.1" "::1"
+        config.lib.this.ip4
+        config.lib.this.ip6
+      ]);
+    };
+
+    authentication = ''
+      host all all ${config.lib.subnet.v4Full} md5
+      host all all ${config.lib.subnet.v6Full} md5
+    '';
   };
 }
