ded59e8980545144468ca7c7c707c999fa0e2146
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-01-08 14:50:02 +0800

    motd: add style

--- modules/system/misc/motd.nix
+++ modules/system/misc/motd.nix
@@ -3,11 +3,11 @@ let
   colorBar = "[40m [41m [42m [43m [44m [45m [46m [47m [100m [101m [102m [103m [104m [105m [106m [107m [40m [41m [42m [43m [44m [45m [46m [47m [100m [101m [102m [103m [104m [105m [106m [107m [0m";
 in {
   users.motd = ''
-      #================================================#
-      #                     WARNING                    #
-      #  Unauthorized personnel accessing this system  #
-      #         will be TERMINATED IMMEDIATELY         #
-      #================================================#
+      #===========================================================#
+      #                          [1m[5m[33mWARNING[0m                          #
+      #      ANY UNAUTHORIZED PERSONNEL accessing this system     #
+      # will be [1m[31mTERMINATED IMMEDIATELY[0m through [93mMEMETIC KILL AGENT[0m #
+      #===========================================================#
 
     Welcome to A1ca7raz's Private Network!
 
