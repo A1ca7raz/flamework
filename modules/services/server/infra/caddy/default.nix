0f8fe7e9a6447938c0ba653f5cad7eda3700479a
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-01-21 02:06:29 +0800

    caddy: fix veth

--- modules/services/server/infra/caddy/default.nix
+++ modules/services/server/infra/caddy/default.nix
@@ -1,7 +1,20 @@
-{ ... }:
+{ config, ... }:
 {
   services.caddy = {
     enable = true;
     acmeCA = "https://pki.insyder/acme/x1/directory";
+  };
+
+  # Reverse proxy netns
+  utils.netns.veth.caddy = {
+    bridge = "0";
+    netns = "proxy";
+    inherit (config.lib.services.caddy) ipAddrs;
+  };
+
+  systemd.services.caddy = {
+    after = [ "netns-veth-caddy.service" ];
+    bindsTo = [ "netns-veth-caddy.service" ];
+    serviceConfig.NetworkNamespacePath = "/run/netns/proxy";
   };
 }
