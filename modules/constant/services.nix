f4852bc4b9c2d533f86f2eb2d581fa6b15d7cd54
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-01-21 02:07:13 +0800

    constant: update services

--- modules/constant/services.nix
+++ modules/constant/services.nix
@@ -3,14 +3,14 @@
   lib.vnet.ipAddrs = [ "198.18.10.254/24" ];
 
   lib.services = {
+    # Infra
     postgresql = { domains = [ "psql.insyder" ]; ipAddrs = [ "198.18.10.1/24" ]; };
-
-    redis = { domains = [ "redis.insyder" ]; ipAddrs = [ "198.18.10.2/24" ]; };
-
+    caddy = { domains = [ "caddy.insyder" ]; ipAddrs = [ "198.18.10.2/24" ]; };
     minio = { domains = [ "s3.insyder" ]; ipAddrs = [ "198.18.10.3/24" ]; };
-
     step-ca = { domains = [ "pki.insyder" ]; ipAddrs = [ "198.18.10.4/24" ]; };
+    redis = { domains = [ "redis.insyder" ]; ipAddrs = [ "198.18.10.5/24" ]; };
 
+    # Services
     authentik = { domains = [ "id.insyder" ]; ipAddrs = [ "198.18.10.10/24" ]; };
   };
 
