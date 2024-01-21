d352f573e693b06db38a432869295860a7186919
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-01-21 12:09:50 +0800

    sing-box: fix auto update

--- modules/services/client/sing-box/default.nix
+++ modules/services/client/sing-box/default.nix
@@ -1,8 +1,9 @@
 { config, lib, pkgs, ... }:
 with lib; let
   enable = true;
-  configFile = "$STATE_DIRECTORY/config.json";
-  subscriptionURI = config.sops.secrets.sing-box.path;
+  configFile = "/var/lib/sing-box/config.json";
+  updateTimeoutMin = "720";
+  subscriptionEnv = config.sops.secrets.sing-box.path;
 
   uiPackage = pkgs.clash-webui-yacd-meta;
 in {
@@ -10,30 +11,16 @@ in {
   sops.secrets.sing-box.mode = "0600";
 
   systemd.services.sing-box = optionalAttrs enable (
-    with pkgs; let
-      startScript = pkgs.writeShellScriptBin "sing-box-start" ''
-        SUB_URI=$1
-        CONF_DIR=${"\$\{STATE_DIRECTORY:-/var/lib/sing-box}"}
-        CONF=${configFile}
-        mkdir -p $CONF_DIR
-
-        curl -L `cat $SUB_URI` -o $CONF
-        chmod 0700 $CONF
-        ${sing-box}/bin/sing-box run -D $CONF_DIR -c $CONF
-      '';
-
+    let
       caps = [ "CAP_NET_ADMIN" "CAP_NET_BIND_SERVICE" ];
     in {
       description = "Sing-box networking service";
-      path = [ coreutils curl ];
-
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
+      path = with pkgs; [ coreutils curl sing-box ];
 
       serviceConfig = {
         Type = "simple";
-        LoadCredential = "subscription_uri:${subscriptionURI}";
-        ExecStart = "${getExe startScript} %d/subscription_uri";
         StateDirectory = "sing-box";
         CapabilityBoundingSet = caps;
         AmbientCapabilities = caps;
@@ -42,10 +29,45 @@ in {
         Group = "sing-box";
         DynamicUser = "yes";
 
+        EnvironmentFile = [ subscriptionEnv ];
+
         # Sing-box auto update
         Restart = "always";
-        RuntimeMaxSec = "12h";
+        RuntimeMaxSec = "${updateTimeoutMin}min";
+
+        OOMPolicy = "kill";
       };
+
+      environment = {
+        SB_WORK_DIR = "/var/lib/sing-box";
+        SB_CONF_FILE = configFile;
+      };
+
+      preStop = ''
+        curl --connect-timeout 5 --retry 3 --retry-delay 1 \
+          -L $SB_SUBSCRIPTION_URI \
+          -o $SB_CONF_FILE
+      '';
+
+      script = ''
+        [[ -d $SB_WORK_DIR ]] || mkdir -p $SB_WORK_DIR
+
+        if [[ -f $SB_CONF_FILE ]] && test `find $SB_CONF_FILE -mmin -2`; then
+          :
+        elif [[ -f $SB_CONF_FILE ]] && test `find $SB_CONF_FILE -mmin -${updateTimeoutMin}`; then
+          curl --connect-timeout 5 \
+            -L $SB_SUBSCRIPTION_URI \
+            -o $SB_CONF_FILE
+        else
+          curl --connect-timeout 5 --retry 3 --retry-delay 1 \
+            -L $SB_SUBSCRIPTION_URI \
+            -o $SB_CONF_FILE
+        fi
+
+        [[ -f $SB_CONF_FILE ]] && chmod 0700 $SB_CONF_FILE || exit 1
+
+        sing-box run -D $SB_WORK_DIR -c $SB_CONF_FILE
+      '';
     }
   );
 
