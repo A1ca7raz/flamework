7b790433cf7ce5eef40fee7794ac0071db0488db
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-01-25 13:32:56 +0800

    utils: add systemd service harden tool

---
+++ utils/lib/systemd.nix
@@ -0,0 +1,37 @@
+lib:
+let
+  hardenSettings.serviceConfig = {
+    LockPersonality = true;
+    MemoryDenyWriteExecute = true;
+    NoNewPrivileges = true;
+    PrivateUsers = true;
+    PrivateDevices = true;
+    PrivateMounts = true;
+    PrivateTmp = true;
+    ProcSubset = "pid";
+    ProtectClock = true;
+    ProtectControlGroups = true;
+    ProtectHome = true;
+    ProtectHostname = true;
+    ProtectKernelLogs = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    ProtectProc = "invisible";
+    ProtectSystem = "strict";
+    RemoveIPC = true;
+    RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" ];
+    RestrictNamespaces = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    UMask = "0077";
+    SystemCallArchitectures = "native";
+    SystemCallErrorNumber = "EPERM";
+    SystemCallFilter = [
+      "@system-service"
+      # Route-chain and OpenJ9 requires @resources calls
+      "~@clock @cpu-emulation @debug @module @mount @obsolete @privileged @raw-io @reboot @swap"
+    ];
+  };
+in {
+  hardenService = x: lib.mkMerge [ hardenSettings x ];
+}
