779440e2072680ceccdc3cf2a0ef06a8f4d68c97
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-02-12 09:37:36 +0800

    latte: use kconfig

--- modules/desktop/plasma/lattedock/default.nix
+++ modules/desktop/plasma/lattedock/default.nix
@@ -1,14 +1,35 @@
 {
-  nixosModule = { tools, user, ... }:
+  nixosModule = { tools, user, config, ... }:
     with tools; {
       utils.kconfig.files.kwinrc.items = [
         (mkItem "ModifierOnlyShortcuts" "Meta" "org.kde.lattedock,/Latte,org.kde.LatteDock,activateLauncherMenu")
       ];
 
+      utils.kconfig.files.lattedockrc.items = [
+        (mkItem "General" "Number of Actions" "0")
+        (mkItem "PlasmaThemeExtended" "outlineWidth" "1")
+        (mkItem "UniversalSettings" "badges3DStyle" "false")
+        (mkItem "UniversalSettings" "canDisableBorders" "true")
+        (mkItem "UniversalSettings" "contextMenuActionsAlwaysShown" "_layouts,_preferences,_quit_latte,_separator1,_add_latte_widgets,_add_view")
+        (mkItem "UniversalSettings" "inAdvancedModeForEditSettings" "true")
+        (mkItem "UniversalSettings" "inConfigureAppletsMode" "true")
+        (mkItem "UniversalSettings" "isAvailableGeometryBroadcastedToPlasma" "true")
+        (mkItem "UniversalSettings" "launchers" "")
+        (mkItem "UniversalSettings" "memoryUsage" "0")
+        (mkItem "UniversalSettings" "metaPressAndHoldEnabled" "false")
+        (mkItem "UniversalSettings" "mouseSensitivity" "2")
+        (mkItem "UniversalSettings" "parabolicSpread" "3")
+        (mkItem "UniversalSettings" "parabolicThicknessMarginInfluence" "1")
+        (mkItem "UniversalSettings" "screenTrackerInterval" "1000")
+        (mkItem "UniversalSettings" "showInfoWindow" "true")
+        (mkItem "UniversalSettings" "singleModeLayoutName" "MyDock")
+        (mkItem "UniversalSettings" "version" "2")
+      ];
+
       environment.overlay = mkOverlayTree user {
         lattedockrc = {
           target = c "lattedockrc";
-          source = ./lattedockrc;
+          source = config.utils.kconfig.files.lattedockrc.path;
         };
       };
 
