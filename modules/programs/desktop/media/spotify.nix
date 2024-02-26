f14f4d062a94812c63bd900752dddc8a262cf129
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-02-26 14:13:07 +0800

    sptlrx: nixify sptlrx configurations

--- modules/programs/desktop/media/spotify.nix
+++ modules/programs/desktop/media/spotify.nix
@@ -25,11 +25,54 @@
         sptlrx                      # Command-line Lyrics
         playerctl
       ];
+
+      xdg.configFile.sptlrx = {
+        target = "sptlrx/config.yaml";
+        text = ''
+          player: mpris
+          timerInterval: 200
+          updateInterval: 2000
+          style:
+            hAlignment: center
+            before:
+              background: ""
+              foreground: "#c1c1c1"
+              bold: true
+              italic: false
+              undeline: false
+              strikethrough: false
+              blink: false
+              faint: false
+            current:
+              background: ""
+              foreground: "#aaaaff"
+              bold: false
+              italic: false
+              undeline: false
+              strikethrough: false
+              blink: false
+              faint: true
+            after:
+              background: ""
+              foreground: ""
+              bold: false
+              italic: false
+              undeline: false
+              strikethrough: false
+              blink: false
+              faint: false
+          pipe:
+            length: 0
+            overflow: ellipsis
+            ignoreErrors: true
+          mpris:
+            players: [ 'spotify', 'spotifyd' ]
+        '';
+      };
     };
 
   nixosModule = { user, tools, ... }:
     with tools; mkPersistDirsModule user [
       (c "spotify")
-      (c "sptlrx")
     ];
 }
