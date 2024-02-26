14f932650ec2478e00adafea088e0bb9575835a0
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-02-26 19:24:44 +0800

    home-manager: enable auto-backup

--- modules/system/home-manager/default.nix
+++ modules/system/home-manager/default.nix
@@ -1,9 +1,14 @@
-{ home, pkgs, ... }:
 {
-  home.sessionPath = [ "$HOME/.local/bin" ];
-  home.stateVersion = "23.05";
+  nixosModule = { ... }: {
+     home-manager.backupFileExtension = "hmbak";
+  };
 
-  # programs.home-manager.enable = true;
-  # https://github.com/nix-community/home-manager/issues/3211
-  home.packages = [ pkgs.home-manager ];
+  homeModule = { pkgs, ... }: {
+    home.sessionPath = [ "$HOME/.local/bin" ];
+    home.stateVersion = "23.05";
+
+    # programs.home-manager.enable = true;
+    # https://github.com/nix-community/home-manager/issues/3211
+    home.packages = [ pkgs.home-manager ];
+  };
 }
