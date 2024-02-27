738f2eca4873c3e61c0203f5a1be6bdad19df856
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-02-28 00:15:36 +0800

    firefox: rollback search engine config

--- modules/programs/desktop/networking/firefox/search.nix
+++
@@ -1,6 +0,0 @@
-{ home, ... }:
-{
-  # Exported from Search Engines Helper
-  programs.firefox.profiles.Default.search.engines = with builtins;
-    fromJSON (readFile ./all-browser-engines.json);
-}
