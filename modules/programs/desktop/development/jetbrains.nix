e2e92e168f20b1c78f26dd14c5cc1166375876b3
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-02-29 19:14:39 +0800

    jetbrains: restore

--- modules/programs/desktop/development/jetbrains.nix
+++ modules/programs/desktop/development/jetbrains.nix
@@ -2,10 +2,10 @@
   homeModule = { pkgs, ... }: {
     home.packages = with pkgs; [
       # Remove all JetBrains software for now due to broken NUR cache.
-      # jb-clion-fixed
-      # jb-datagrip-fixed
-      # jb-idea-fixed
-      # jb-pycharm-fixed
+      jb-clion-fixed
+      jb-datagrip-fixed
+      jb-idea-fixed
+      jb-pycharm-fixed
     ];
   };
 
