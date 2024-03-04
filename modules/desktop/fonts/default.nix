64a9349c0cf49afcb8b706f28d355a64b86ed879
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-03-04 11:01:59 +0800

    fonts: rename defaultFonts

--- modules/desktop/fonts/default.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  fonts.fontconfig.defaultFonts = {
-    serif = [ "Source Han Serif SC" "Twitter Color Emoji" "Blobmoji" ];
-    sansSerif = [ "Source Han Sans SC" "Twitter Color Emoji" "Blobmoji" ];
-    monospace = [ "Source Han Mono SC" "Twitter Color Emoji" "Blobmoji" ];
-    emoji = [ "Twitter Color Emoji" "Blobmoji" ];
-  };
-}
---
+++ modules/desktop/fonts/fonts.nix
@@ -0,0 +1,9 @@
+{ ... }:
+{
+  fonts.fontconfig.defaultFonts = {
+    serif = [ "Source Han Serif SC" "Blobmoji" "Twitter Color Emoji" ];
+    sansSerif = [ "Source Han Sans SC" "Blobmoji" "Twitter Color Emoji" ];
+    monospace = [ "Source Han Mono SC" "Blobmoji" "Twitter Color Emoji" ];
+    emoji = [ "Twitter Color Emoji" "Blobmoji" ];
+  };
+}
