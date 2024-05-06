f9f3c9c591e62e9886c224814a91717c2e8c855e
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-05-06 23:36:37 +0800

    constant: init

--- constant/services.nix
+++ constant/services.nix
@@ -1,8 +1,7 @@
-{ lib, config, tools, ... }:
 {
-  lib.vnet.ipAddrs = [ "198.18.10.254/24" ];
+  vnet.ipAddrs = [ "198.18.10.254/24" ];
 
-  lib.services = {
+  services = {
     # Infra
     postgresql = { domains = [ "psql.insyder" ]; ipAddrs = [ "198.18.10.1/24" ]; };
     caddy = { domains = [ "caddy.insyder" ]; ipAddrs = [ "198.18.10.2/24" ]; };
@@ -13,13 +12,4 @@
     gitea = { domains = [ "git.insyder" ]; ipAddrs = [ "198.18.10.11/24" ]; };
     ocis = { domains = [ "drive.insyder" ]; ipAddrs = [ "198.18.10.12/24" ]; };
   };
-
-  networking.hosts =
-    with lib; let
-      cfg = config.lib.services;
-    in foldlAttrs (acc: n: v:
-      (foldl (acc_: ip:
-        acc_ // { ${ip} = v.domains; }
-      ) {} (tools.removeCIDRSuffixes v.ipAddrs)) // acc
-    ) {} cfg;
 }
--- modules/constant/global.nix
+++
@@ -1,6 +0,0 @@
-{ lib, ... }:
-{
-  lib.global = {
-    type = lib.mkDefault "server";
-  };
-}
