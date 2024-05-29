5798dd897c3c7b1500eb48e1718144785ac7a84d
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-05-29 13:37:52 +0800

    hosts: service domains

---
+++ modules/hosts/services/default.nix
@@ -0,0 +1,14 @@
+{ config, lib, var, ... }:
+with lib;
+if config.services.knot.enable
+then {
+  # TODO: Knot domains
+} else {
+  networking.hosts = mapAttrs'
+    (n: v:
+      nameValuePair
+        (cidr (elemAt v.ipAddrs 0)).ip
+        v.domains
+    )
+    var.services;
+}
