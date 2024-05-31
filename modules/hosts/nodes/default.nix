9441d8cb41d52214be966f87c3b54751a86d5472
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-05-31 21:05:48 +0800

    hosts: fix encrypted hosts

--- modules/hosts/nodes/default.nix
+++ modules/hosts/nodes/default.nix
@@ -1,21 +1,21 @@
 { config, ... }:
 let
-  readSecrets = part: builtins.foldl'
-    (acc: e: acc // { "${part}/${e}" = { sopsFile = ./secrets.yml; }; })
-    {};
-  
-  pl = config.sops.placeholder;
-in {
-  sops.secrets = readSecrets "nodes" [
-    "a" "b" "c"
-    "s1" "s2"
-  ];
+  prefix = "nodes";
 
-  utils.encrypted.hosts = {
-    "${pl."nodes/a"}" = [ "a.node" ];
-    "${pl."nodes/b"}" = [ "b.node" ];
-    "${pl."nodes/c"}" = [ "c.node" ];
-    "${pl."nodes/s1"}" = [ "s1.node" ];
-    "${pl."nodes/s2"}" = [ "s2.node" ];
-  };
+  nodes = [
+    "a"
+    "b"
+    "c"
+    "s1" "s2"
+  ];  
+in {
+  sops.secrets = builtins.foldl'
+    (acc: e:
+      acc // { "${prefix}/${e}" = { sopsFile = ./secrets.yml; }; }
+    ) {} nodes;
+
+  utils.encrypted.reversedHosts = builtins.foldl'
+    (acc: e:
+      acc // { "${e}.node" = config.sops.placeholder."${prefix}/${e}"; }
+    ) {} nodes;
 }
---
+++ modules/hosts/services/dns.nix
@@ -0,0 +1,7 @@
+{ dns, services }:
+with dns.lib.combinators;
+let
+  
+in dns.lib.toString "insyder" {
+  # TODO: dns & common dns
+}
