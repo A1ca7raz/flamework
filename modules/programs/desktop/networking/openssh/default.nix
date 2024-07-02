9c32b05df0e69b3720e3145ee9b2f521ceef7abc
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-07-02 15:12:40 +0800

    openssh: set ssh port as constant

--- modules/programs/desktop/networking/openssh/default.nix
+++ modules/programs/desktop/networking/openssh/default.nix
@@ -2,13 +2,27 @@
   nixosModule = { user, lib, ... }:
     with lib; mkPersistDirsModule user [ ".ssh" ];
 
-  homeModule = { config, ... }: {
+  homeModule = { config, var, ... }: {
     utils.secrets.sshconfig.enable = true;
 
     programs.ssh = {
       enable = true;
       # fix %r
       includes = [ "/run/user/1000/secrets/sshconfig" ];
+      
+      matchBlocks = {
+        gh = {
+          hostname = "github.com";
+          user = "git";
+        };
+        gl = {
+          hostname = "gitlab.com";
+          user = "git";
+        };
+        "*.node" = {
+          port = var.port.ssh;
+        };
+      };
     };
   };
 }
--- modules/services/server/openssh/default.nix
+++ modules/services/server/openssh/default.nix
@@ -1,14 +1,12 @@
-{ lib, ... }:
-let
-  PORTS = [ 48422 ];
-in {
+{ lib, var, ... }:
+{
   services.openssh = {
     enable = true;
     settings = {
       PasswordAuthentication = false;
       PermitRootLogin = if lib.utils.isServer then "prohibit-password" else "no";
     };
-    ports = PORTS;
+    ports = [ var.port.ssh ];
     hostKeys = [
       { path = "/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
       { path = "/etc/ssh/ssh_host_rsa_key"; type = "rsa"; }
