3d6f8a747f54c7e485c5685dbefa2876ee4a0f46
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-07-19 21:37:31 +0800

    openssh: add aosc config and known_hosts

--- modules/programs/desktop/networking/openssh/default.nix
+++ modules/programs/desktop/networking/openssh/default.nix
@@ -7,7 +7,13 @@
 
     programs.ssh = {
       enable = true;
-      includes = [ config.sops.secrets.sshconfig.path ];
+      includes = [
+        config.sops.secrets.sshconfig.path
+        (builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/AOSC-Dev/Buildbots/main/ssh_config";
+          sha256 = "sha256:1lh1hasggk739mzy48rymwa85d7l8db15gw6524ah9xy11m83f9i";
+        })
+      ];
       
       matchBlocks = {
         gh = {
@@ -22,6 +28,14 @@
           port = var.port.ssh;
         };
       };
+    };
+
+    home.file.aosc-known-hosts = {
+      source = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/AOSC-Dev/Buildbots/main/ssh_known_hosts";
+        sha256 = "sha256:0lha6r8kny27im1f1zn306d84vv4zc99k68vdy7hvwd9d274krpx";
+      };
+      target = ".ssh/known_hosts.d/aosc";
     };
   };
 }
