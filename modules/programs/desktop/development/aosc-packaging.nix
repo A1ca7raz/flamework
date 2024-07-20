dab8231c3f01047ad5716d19a2c24ff23893c61d
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-07-21 00:18:05 +0800

    aosc: add squashfsTools for ciel

--- modules/programs/desktop/development/aosc-packaging.nix
+++ modules/programs/desktop/development/aosc-packaging.nix
@@ -1,6 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   environment.systemPackages = with pkgs; [
     ciel-latest
+    squashfsTools # necessary dep
   ];
+
+  environment.persistence."/nix/persist".users.root = {
+    home = lib.mkDefault  "/root";
+    directories = [
+      "ciel"
+    ];
+  };
 }
