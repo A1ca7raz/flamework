777164a87eaf567612c9a06cfae8e689c455bff2
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-07-20 23:07:55 +0800

    adb: persist state

--- modules/programs/desktop/development/android-tools.nix
+++ modules/programs/desktop/development/android-tools.nix
@@ -1,6 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, user, lib, ... }:
 {
   # Android-Tools
   programs.adb.enable = true;
   services.udev.packages = [ pkgs.android-udev-rules ];
+
+  environment.persistence = lib.mkPersistDirsTree user [
+    ".android"
+  ];
 }
