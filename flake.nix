8805f17c4e66af651a1ee97205515d884a2e9759
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-08-27 14:40:51 +0800

    flake: add nixConfig

--- flake.nix
+++ flake.nix
@@ -1,6 +1,18 @@
 {
   description = "Flamework 3";
 
+  nixConfig = {
+    # https://github.com/A1ca7raz/nurpkgs/blob/main/config.nix
+    extra-substituters = [
+      "https://cache.garnix.io"
+      "https://a1ca7raz-nur.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "a1ca7raz-nur.cachix.org-1:twTlSh62806B8lfG0QQzge4l5srn9Z8/xxyAFauOZnQ="
+    ];
+  };
+
   inputs = {
     # Use inputs from my NUR flake
     nur.url = "github:A1ca7raz/nurpkgs";
