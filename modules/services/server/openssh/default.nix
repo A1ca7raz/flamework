c93981ebac1d5212298cd3a93c97da19222c5d79
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-11-17 09:40:51 +0800

    openssh: update

--- modules/services/server/openssh/default.nix
+++ modules/services/server/openssh/default.nix
@@ -9,15 +9,15 @@
     ports = [ var.port.ssh ];
     hostKeys = [
       { path = "/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
-      { path = "/etc/ssh/ssh_host_rsa_key"; type = "rsa"; }
+      { path = "/etc/ssh/ssh_host_rsa_key"; type = "rsa"; bits = 4096; }
     ];
     # extraConfig = ''
-    #   TrustedUserCAKeys /etc/ssh/ssh_user_ca.pub
+    #   TrustedUserCAKeys /etc/ssh/trusted_user_ca
     # '';
   };
 
   # environment.etc.sshUserCA = {
-  #   target = "ssh/ssh_user_ca.pub";
+  #   target = "ssh/trusted_user_ca";
   #   text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIImEnstzrNsASzPhILySuXHjeyA84Hv0U1ini3w/4JBn";
   # };
 }
