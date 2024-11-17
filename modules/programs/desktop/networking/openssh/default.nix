d8b7b73eed080ec9b2eac70ba78f84361dac20be
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-11-17 09:51:17 +0800

    openssh: update AOSC infra

--- modules/programs/desktop/networking/openssh/default.nix
+++ modules/programs/desktop/networking/openssh/default.nix
@@ -8,7 +8,7 @@
       includes = [
         (builtins.fetchurl {
           url = "https://raw.githubusercontent.com/AOSC-Dev/Buildbots/main/ssh_config";
-          sha256 = "sha256:1lh1hasggk739mzy48rymwa85d7l8db15gw6524ah9xy11m83f9i";
+          sha256 = "sha256:15rn516vgdih0kqc08a33dqy6nivm5pgvmqi9mxjm2pnsxgffnal";
         })
       ];
 
@@ -37,7 +37,7 @@
     home.file.aosc-known-hosts = {
       source = builtins.fetchurl {
         url = "https://raw.githubusercontent.com/AOSC-Dev/Buildbots/main/ssh_known_hosts";
-        sha256 = "sha256:0lha6r8kny27im1f1zn306d84vv4zc99k68vdy7hvwd9d274krpx";
+        sha256 = "sha256:123mmjhihs89kczfjsnwfc5bfv4m0lfh6qzs7ck20v544qb2l2cw";
       };
       target = ".ssh/known_hosts.d/aosc";
     };
