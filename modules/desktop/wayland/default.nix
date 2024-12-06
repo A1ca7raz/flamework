a6a31f92404a1da6819fc1a0d93d9300a65021ca
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-12-06 23:25:51 +0800

    wayland: persist xdg portal states

--- modules/desktop/wayland/default.nix
+++ modules/desktop/wayland/default.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{ user, lib, ... }:
 {
-  environment.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = "1";   # Firefox Wayland
+  environment = {
+    sessionVariables = {
+      MOZ_ENABLE_WAYLAND = "1";   # Firefox Wayland
+    };
+
+    persistence = with lib; mkPersistDirsTree user [
+      (ls "flatpak")  # XDG Desktop Portal
+    ];
   };
 }
