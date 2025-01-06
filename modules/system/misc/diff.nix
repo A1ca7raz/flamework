c0f8819c0358aeeb95769d809f619115ba7bfd9f
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-01-06 13:58:23 +0800

    nix: add change report in activation script

---
+++ modules/system/misc/diff.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+{
+  system.activationScripts.diff = ''
+    if [[ -e /run/current-system ]]; then
+      ${pkgs.nix}/bin/nix store diff-closures /run/current-system "$systemConfig"
+    fi
+  '';
+}
