ee072f23252523afefac84589156f8d4558338d9
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-05-09 00:40:42 +0800

    lib: add serviceHarden

--- lib/module_helper.nix
+++ lib/module_helper.nix
@@ -1,4 +1,4 @@
-args:
+lib:
 rec {
   mkPersistDirsTree = user: dirs: {
     "/nix/persist".users.${user}.directories = dirs;
@@ -26,5 +26,43 @@ rec {
 
   mkHomePackagesModule = list: {
     home = mkHomePackagesTree list;
+  };
+
+  # https://github.com/xddxdd/nixos-config/blob/master/helpers/fn/service-harden.nix
+  hardenService = lib.mapAttrs (_k: lib.mkOptionDefault) {
+    AmbientCapabilities = "";
+    CapabilityBoundingSet = "";
+    LockPersonality = true;
+    MemoryDenyWriteExecute = true;
+    NoNewPrivileges = true;
+    PrivateDevices = true;
+    PrivateMounts = true;
+    PrivateTmp = true;
+    ProcSubset = "pid";
+    ProtectClock = true;
+    ProtectControlGroups = true;
+    ProtectHome = true;
+    ProtectHostname = true;
+    ProtectKernelLogs = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    ProtectProc = "invisible";
+    ProtectSystem = "strict";
+    RemoveIPC = true;
+    RestrictAddressFamilies = [
+      "AF_UNIX"
+      "AF_INET"
+      "AF_INET6"
+    ];
+    RestrictNamespaces = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    SystemCallArchitectures = "native";
+    SystemCallErrorNumber = "EPERM";
+    SystemCallFilter = [
+      "@system-service"
+      # Route-chain and OpenJ9 requires @resources calls
+      "~@clock @cpu-emulation @debug @module @mount @obsolete @privileged @raw-io @reboot @swap"
+    ];
   };
 }
