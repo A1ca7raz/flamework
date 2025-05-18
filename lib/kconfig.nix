c40654e6345f6f14a667ab91481d4ca6dcace60c
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-05-18 21:16:23 +0800

    lib: add convertItemsToKconfig

--- lib/kconfig.nix
+++ lib/kconfig.nix
@@ -1,5 +1,30 @@
 lib:
-{
+let
+  inherit (lib)
+    updateManyAttrsByPath
+  ;
+
+  inherit (builtins)
+    isList
+    map
+    concatStringsSep
+  ;
+in {
+  convertItemsToKconfig = items:
+    let
+      updatePaths = map (i:
+        let
+          _g = if isList i.g then i.g else [ i.g ];
+          group = concatStringsSep "/" _g;
+        in {
+          path = [ group i.k ];
+          update = old: i.v;
+        }
+      ) items;
+    in
+      updateManyAttrsByPath updatePaths {}
+    ;
+
   mkItem = g: k: v: { inherit g k v; };
   mkRule = f: g: k: v: { inherit f g k v; };
 }
